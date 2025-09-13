8795055e95671ee4cddcce62158c1201e2008d79
TharaniBhagya <herathbhagya100@gmail.com>
2025-09-13 21:45:24 +0530

    solved errors before release

--- lib/views/screens/pet_form_screen.dart
+++ lib/views/screens/pet_form_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:scooby_app_new/controllers/pet_form_controller.dart';
 import 'package:scooby_app_new/controllers/pet_service.dart';
